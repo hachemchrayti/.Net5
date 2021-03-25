e05512dd94c2d7a87489ee154b8e30df71fc60b8
hachemchrayti <hachemfst@gmail.Com>
2021-03-25 22:22:41 +0100

    Strings(hows strings are immutable) & why to use StringBuilder

--- Net5CodingProject/Program.cs
+++ Net5CodingProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
 
 namespace Net5CodingProject
 {
@@ -6,19 +8,58 @@ namespace Net5CodingProject
     {
         static void Main(string[] args)
         {
-            // Displaying double quotes in c#
-            string Name = "\"Dotnettutorials\"";
-            Console.WriteLine(Name);
-            // Displaying new line character in c#
-            Name = "One\nTwo\nThree";
-            Console.WriteLine(Name);
-            // Displaying new line character in c#
-            Name = "c:\\Pranaya\\Dotnettutorials\\Csharp";
-            Console.WriteLine(Name);
-            // C# verbatim literal
-            Name = @"c:\Pranaya\Dotnettutorials\Csharp";
-            Console.WriteLine(Name);
-            Console.ReadKey();
+
+            string str = "";
+            Console.WriteLine("Loop Started");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 30000000; i++)
+            {
+                str = Guid.NewGuid().ToString();
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Loop Ended");
+            Console.WriteLine("Loop Exceution Time in MS :" + stopwatch.ElapsedMilliseconds);
+
+            Console.WriteLine("-------------------------------------------------------------");
+
+            int ctr = 0;
+            Console.WriteLine("Loop Started");
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 30000000; i++)
+            {
+                ctr = ctr + 1;
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Loop Ended");
+            Console.WriteLine("Loop Exceution Time in MS :" + stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("-------------------------------------------------------------");
+
+            var str1 = "";
+            Console.WriteLine("Loop Started");
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 30000; i++)
+            {
+                str = "DotNet Tutorials" + str;
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Loop Ended");
+            Console.WriteLine("Loop Exceution Time in MS :" + stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("-------------------------------------------------------------");
+
+            StringBuilder stringBuilder = new StringBuilder();
+            Console.WriteLine("Loop Started");
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 30000; i++)
+            {
+                stringBuilder.Append("DotNet Tutorials");
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Loop Ended");
+            Console.WriteLine("Loop Exceution Time in MS :" + stopwatch.ElapsedMilliseconds);
         }
     }
 }
